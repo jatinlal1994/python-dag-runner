bfd346080c49e90265354dc3e27de5633f9ab2cd
Jatin Lal <jatinlal1994@gmail.com>
2024-02-27 00:16:57 +0000

    Added github URL to pypi package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "readme.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Run tasks as a DAG'
 LONG_DESCRIPTION = 'Allows you to run multiple processes as DAG'
 
@@ -22,6 +22,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
+    url='https://github.com/jatinlal1994/python-dag-runner',
     install_requires=['networkx'],
     keywords=['python', 'dag', "runner"],
     classifiers=[
