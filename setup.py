d799e50e4954328a5616d7d8045ac79c7523c60e
Jatin Lal <jatinlal1994@gmail.com>
2024-02-28 00:40:17 +0000

    Updated version in setup file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "readme.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Run tasks as a DAG'
 LONG_DESCRIPTION = 'Allows you to run multiple processes as DAG'
 
